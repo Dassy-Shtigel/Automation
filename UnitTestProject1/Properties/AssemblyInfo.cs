338e14a5c29fa86fece9ca2cd1dfe8852a0b284e
Dassi Shtigel <dshtigel@gmail.com>
2020-12-09 00:49:25 +0200

    First commit

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3aaee3dc-b663-42b8-a4ff-48d3807f29ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using UnitTestProject1.Pages;
+using System.Diagnostics;
+using UnitTestProject1.Infra;
+
+
+
+namespace UnitTestProject1
+{
+   
+
+    
+
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            LoginPage login = new LoginPage();
+            LoginInfra loginInfra = new LoginInfra();
+            //Launch google
+            driver.Navigate().GoToUrl("https://dshtigel.netlify.app/");
+
+           
+
+
+
+            //find the search bar and enter the text
+            //IWebElement emailField = driver.FindElement(By.Id("email"));
+            loginInfra.InsertEmail("dshtigel@gmail.com");
+            login.GetEmailField(driver).SendKeys(Keys.Enter);
+            
+            
+            login.GetPassField(driver).SendKeys("123456");
+            login.GetPassField(driver).SendKeys(Keys.Enter);
+
+            //Find result by LinkText and click
+            IWebElement ResultElement = driver.FindElement(By.CssSelector("div > div.Income_balance__3PrIs > p:nth-child(1)"));
+            ResultElement.Click();
+
+            string expectedPageTitle = "MMM";
+            string actualPageTitle = driver.Title;
+
+            if (expectedPageTitle == actualPageTitle)
+            {
+                Trace.WriteLine("done");
+            } else
+            {
+                Trace.WriteLine("failed");
+            }
+
+        }
+    }
+}
+
